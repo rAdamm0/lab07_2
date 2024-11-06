8dd3a432271051137906bf057eea9e39d84efb7c
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-11-06 14:41:05 +0100

    Created the enum class Month with its constructor

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -8,6 +8,31 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    public enum Month{
+        JAN(31),
+        FEB(28),
+        MAR(31),
+        APR(30),
+        MAY(31),
+        JUN(30),
+        JUL(31),
+        AUG(31),
+        SEP(30),
+        OCT(31),
+        NOV(30),
+        DEC(31);
+
+        private final int dayNumb;
+        
+
+        Month(int i) {
+            this.dayNumb = i;
+        }
+        
+     
+       
+        
+    }
 
     @Override
     public Comparator<String> sortByDays() {
